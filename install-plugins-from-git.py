196245e40b56e917e0677d9f5d651d84769ef2eb
Aurélien Bompard <aurelien@bompard.org>
2023-10-09 13:25:58 +0200

    Add a script to install a maubot plugin locally from git
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

---
+++ install-plugins-from-git.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import os
+import shutil
+from argparse import ArgumentParser
+from contextlib import contextmanager
+from pathlib import Path
+from urllib.parse import urlparse
+from subprocess import run
+from tempfile import TemporaryDirectory
+
+
+def run_cmd(cmd):
+    return run(cmd, check=True, text=True)
+
+
+@contextmanager
+def pushd(new_dir):
+    previous_dir = os.getcwd()
+    os.chdir(new_dir)
+    try:
+        yield
+    finally:
+        os.chdir(previous_dir)
+
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument("-b", "--branch", help="Branch to checkout")
+    parser.add_argument("-o", "--output", help="Maubot's plugin directory")
+    parser.add_argument("git_url", nargs="+", help="Git URL to checkout")
+    return parser.parse_args()
+
+
+def install_plugin(git_url, branch, output):
+    cmd = ["git", "clone", "--depth", "1", "--single-branch", git_url]
+    if branch:
+        cmd[2:2] = ["-b", branch]
+    run_cmd(cmd)
+    clone_dir = Path(urlparse(git_url).path).stem
+    with pushd(clone_dir):
+        with TemporaryDirectory() as tmpdirname:
+            run(["mbc", "build", "-o", tmpdirname])
+            for plugin in Path(tmpdirname).glob("*.mbp"):
+                shutil.move(plugin, output)
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    for git_url in args.git_url:
+        install_plugin(git_url, args.branch, args.output)
